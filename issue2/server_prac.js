6d799ab001a02cf046230abad3026e3cc19508cf
HaekyungSeong <haekyungs95@gmail.com>
2023-03-20 22:31:15 +0900

    server 적요로 설명 추가

--- issue2/server_prac.js
+++ issue2/server_prac.js
@@ -16,30 +16,46 @@ const form = fs.readFileSync('index_03.text');
 // }
 
 const server = http.createServer(function (request, response) {
+  // 서버를 만듦
   if (request.method === "GET" && request.url === "/") {
+    // 요청이 GET 이고, url 이 / 이면 실행해
     response.writeHead(200, { 'Content-Type': 'text/html' });
+    // status는 200 이고, type은 text.html.
     response.write(httphead + form + httptail);
+    // 위에 만들었던 애들 조합으로 html을 조합.
     response.end();
+    // 반응 끝~
   }
   if (request.method === "POST" && request.url.startsWith("/file")) {
+    // 요청이 POST 이고, url시작에 /file이 함께 있을 경우
     let a = "";
+    // 빈 변수를 하나 선언
     request.on("data", (data) => {
+      // 요청이 켜지고, 데이터받으면, 콜백함수 실행
       a += data;
+      // a 변수에 data 담기
       // console.log(data)
     })
     request.on("end", () => {
+      // 요청이 끝날 때,
       let fileName = a.split("=")[1].split("&")[0];
+      // a변수 데이터 중 두번째 = 뒤면서 첫번째&까지만 filename에 담기
       let fileInText = a.split("=")[2];
+      // a변수 데이터 중 세번째 = 뒤
       writeFileSync("./" + fileName + ".text", fileInText);
+      // 그 이름과 내용으로 파일 생성
       response.writeHead(200, { 'Content-Type': 'text/html' });
+      // status는 200 이고, type은 text.html.
       const page = httphead + form + `<p> ${fileName} 이름의 파일이 생성되었습니다. 파일 내에는 ${fileInText} 입니다.</p>` + httptail;
+      // PAGE 라는 변수에 html 생긴거 담기.
       // response.write(a)
       response.write(page);
+      // 새기기.
       // 기존데이터를 지워주고 새로새긴다는 절차가 필요함.
       // BOM API의 "히스토리"를 건드려야함.=> 따로 검증을 해보기.
       // mount, unmount 
       response.end();
-
+      // 반응 끝~
     })
   }
 })
