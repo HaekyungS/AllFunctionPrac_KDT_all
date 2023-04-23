5089b48c96f5b6751e783b0e0d65f3b8aace2525
HaekyungSeong <haekyungs95@gmail.com>
2023-04-23 19:55:39 +0900

    test 파일

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-const test = (a, b) =>{
-  return a+b
-};
+import fs from 'fs'
+import qs from 'qs'
+// const test = (a, b) =>{
+//   return a+b
+// };
 
-export default test;
+// export default test;
+
+const array = [1,2,3,4]
+let map = array.every((e)=>{return e<3;})
+
+console.log(map)
